95ce8f8b3141b790cf7571323889d468b246adb9
ricardoriodi <ricardo.riodi@gmail.com>
2020-01-25 21:22:37 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/riodi/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.riodi.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.riodi.cursomc.domain.Pedido;
+import com.riodi.cursomc.repositories.PedidoRepository;
+import com.riodi.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(    "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName())); 
+		
+	}
+
+}
