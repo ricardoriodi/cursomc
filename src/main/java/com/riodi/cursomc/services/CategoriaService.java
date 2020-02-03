a468cf2f76df4180fbd4ba49e56e980a5475f9ae
ricardoriodi <ricardo.riodi@gmail.com>
2020-02-03 06:13:14 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/riodi/cursomc/services/CategoriaService.java
+++ src/main/java/com/riodi/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.riodi.cursomc.domain.Categoria;
+import com.riodi.cursomc.dto.CategoriaDTO;
 import com.riodi.cursomc.repositories.CategoriaRepository;
 import com.riodi.cursomc.services.exceptions.DataIntegrityException;
 import com.riodi.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,8 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
-
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
 }
