d2339fb55efb5a343163f191331a6d035cbaa360
ricardoriodi <ricardo.riodi@gmail.com>
2020-02-16 22:17:08 -0300

    Validação customizada: CPF ou CNPJ na inserção de cliente

--- src/main/java/com/riodi/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/riodi/cursomc/dto/ClienteNewDTO.java
@@ -2,22 +2,46 @@ package com.riodi.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.riodi.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	@NotEmpty (message="Preenchimento obrigatório")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Email(message="E-mail inválido")	
 	private String email;
-	private String cpfOuConpj;
-	private Integer tipo;
+
+	@NotEmpty(message="Preenchimento obrigatório")	
+	private String cpfOuCnpj;
 	
+	private Integer tipo;
+
+	@NotEmpty(message="Preenchimento obrigatório")	
 	private String logradouro;
+	
+	@NotEmpty(message="Preenchimento obrigatório")	
 	private String numero;
+	
 	private String complemento;
 	private String bairro;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String cep;	
 	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String telefone1;
+	
 	private String telefone2;
 	private String telefone3;
 	
@@ -42,12 +66,12 @@ public class ClienteNewDTO implements Serializable {
 		this.email = email;
 	}
 
-	public String getCpfOuConpj() {
-		return cpfOuConpj;
+	public String getCpfOuCnpj() {
+		return cpfOuCnpj;
 	}
 
-	public void setCpfOuConpj(String cpfOuConpj) {
-		this.cpfOuConpj = cpfOuConpj;
+	public void setCpfOuCnpj(String cpfOuCnpj) {
+		this.cpfOuCnpj = cpfOuCnpj;
 	}
 
 	public Integer getTipo() {
